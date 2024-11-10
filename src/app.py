d5e6d7d3ae4fc2640a246803867263110d75b951
LexoBrunett <56777101+LexoBrunett@users.noreply.github.com>
2024-11-10 19:07:54 +0000

    corrections

--- src/app.py
+++ src/app.py
@@ -5,8 +5,37 @@ engine = db_connect()
 
 import pandas as pd
 
+import pandas as pd
+
 total_data = pd.read_csv("https://raw.githubusercontent.com/4GeeksAcademy/decision-tree-project-tutorial/main/diabetes.csv")
+total_data = total_data.drop_duplicates().reset_index(drop = True)
 total_data.head()
+
+# No normalization of variables is needed for training this model
+
+# Feature selection
+
+from sklearn.feature_selection import SelectKBest
+from sklearn.model_selection import train_test_split
+
+X = total_data.drop("Outcome", axis = 1)
+y = total_data["Outcome"]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 42)
+
+selection_model = SelectKBest(k = 7)
+selection_model.fit(X_train, y_train)
+
+selected_columns = X_train.columns[selection_model.get_support()]
+X_train_sel = pd.DataFrame(selection_model.transform(X_train), columns = selected_columns)
+X_test_sel = pd.DataFrame(selection_model.transform(X_test), columns = selected_columns)
+
+X_train_sel.head()
+X_test_sel.head()
+train_data = X_train_sel
+test_data = X_test_sel
+
+train_data.head()
 
 X_train = train_data.drop(["Outcome"], axis = 1)
 y_train = train_data["Outcome"]
